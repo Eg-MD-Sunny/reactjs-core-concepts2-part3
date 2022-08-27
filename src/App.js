bfb1544b870fd0565eff8f971b41c48b44feb3e1
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-08-27 22:29:22 +0600

    Data Load From API Using UseEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App() {	
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		{/* <Counter></Counter> */}
+		<ExternalUsers></ExternalUsers>
     </div>
   );
 }
+//Component Create #01
+function ExternalUsers(){
+	const [users, setUsers] = useState([]);
+	useEffect(()=>{
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(res=>res.json())
+		.then(data=>setUsers(data))
+	},[])
+	return (
+		<div>
+			<h2>External Users</h2>
+			<p>{users.length}</p>
+			{
+			// users.map(user=><li>{user.name}</li>)
+			users.map(user=><User name={user.name} email={user.email}></User>)
+			}
+		</div>
+	)
+}
+//Component Create #02
+function User(props){
+	return (
+		<div style={{border:'2px solid red', margin:'20px'}}>
+			<h3>Name: {props.name}</h3>
+			<p>Email: {props.email}</p>
+		</div>
+	)
+}
+
+
+
+
+
+
+//Cretate Counter Component
+// function Counter(){
+// 	const [count,setCount] = useState(0);
+	
+// 1st way	
+	// const increaseCount = () =>{
+	// 	const newValue = count + 1;
+	// 	setCount(newValue);
+	// }
+
+// 2nd way		
+// 	const increaseCount = () => setCount(count + 1);
+// 	const decreaseCount = () => setCount(count - 1);
+
+// 	return (
+// 		<div>
+// 			<h1>Count: {count}</h1>
+// 			<button onClick={increaseCount}>Increase</button>
+// 			<button onClick={decreaseCount}>Decrease</button>
+// 		</div>
+// 	)
+// }
+// const products = [
+// 	{name:'Laptob', price: 153000},
+// 	{name:'phone', price: 93000},
+// 	{name:'Watch', price: 33000},
+// 	{name:'Tablat', price: 5000},
+// ]
+
+// {
+// 	products.map(product=>{
+// 		return <Product name={product.name} price={product.price}></Product>
+// 	})
+// }
+// <Product name="Laptop" price="47000"></Product>
+// <Product name="Phone" price="75000"></Product>
+// <Product name="Watch" price="3500"></Product>
+
+//Component Create
+// function Product(props){
+// 	return (
+// 		<div className="product">
+// 			<h3>Name: {props.name}</h3>
+// 			<p>Price: {props.price}</p>
+// 		</div>
+// 	)
+// }
 
 export default App;
+ 
